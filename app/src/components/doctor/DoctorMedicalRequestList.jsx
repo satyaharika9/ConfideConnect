4a0497cd5f7c0a0989c0bf69cb16a156e68abf71
Chethana Benny <benny.c@northeastern.edu>
2024-04-18 20:42:30 -0400

    handle create, delete, expand of med requests and lab requests of doctor and lab

--- app/src/components/doctor/DoctorMedicalRequestList.jsx
+++ app/src/components/doctor/DoctorMedicalRequestList.jsx
@@ -1,50 +1,57 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Tooltip, Button } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, 
+        Paper, Tooltip, Button, Typography, Modal, Box, IconButton, Input } from '@mui/material';
 import ChatIcon from '@mui/icons-material/Chat';
-import GetAppIcon from '@mui/icons-material/GetApp';
 import DeleteIcon from '@mui/icons-material/Delete';
+import CloseIcon from '@mui/icons-material/Close';
 
 import medicalRequestService from '../../services/medicalrequestService';
-import doctorService from "../../services/doctorService";
 
 
-const DoctorMedicalRequestList = () => {
+const DoctorMedicalRequestList = ({fetchData, medicalRequests}) => {
 
-    const user = useSelector((state) => state.user);
-
-    const [medicalRequests, setMedicalRequests] = useState([]);
+    const [openModal, setOpenModal] = useState(false);
+    const [selectedRequest, setSelectedRequest] = useState(null);
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const medicalrequests = await medicalRequestService.getMedicalRequestsForDoctor(user.user.doctorId);
-                const medicalRequestsWithDoctorDetails = await Promise.all(
-                    medicalrequests.map(async medicalrequest => {
-                        const doctor = await doctorService.getDoctor(medicalrequest.doctorId);
-                        return { medicalrequest, doctor };
-                    })
-                );
-                console.log("requestsWithDoctorsDetails : ", medicalRequestsWithDoctorDetails);
-                setMedicalRequests(medicalRequestsWithDoctorDetails);
-            } catch (error) {
-                console.error('Error fetching medical requests or doctors:', error);
-            }
-        };
-
-        fetchData();
+        fetchData('doctor_medical_requests');
     }, []);
 
-    const handleChatClick = () => {
+    const handleChatClick = (e) => {
         console.log('Chat button clicked');
+        e.stopPropagation();
     };
 
-    const handleDeleteClick = () => {
+    const handleDeleteClick = async (e, medicalRequestId) => {
         console.log('Delete button clicked');
+        e.stopPropagation();
+        try {
+            const resp = await medicalRequestService.deleteMedicalRequest(medicalRequestId);
+            console.log("delete resp : ", resp);
+            fetchData('doctor_medical_requests');
+        } catch (error) {
+            console.error('Error deleting medical request:', error);
+        }
+        setOpenModal(false);
+    };
+
+    const handleFileUpload = () => {
+        console.log('File Upload ');
+    };
+
+    const handleRowClick = (request) => {
+        setSelectedRequest(request);
+        setOpenModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+        setSelectedRequest(null);
     };
 
     return (
-        <TableContainer component={Paper} sx={{ marginTop: '30px' }}>
+        <>
+        <TableContainer component={Paper} sx={{ marginTop: '30px', maxHeight: '430px' }}>
             <Table aria-label="medical request table">
                 <TableHead>
                     <TableRow>
@@ -58,19 +65,28 @@ const DoctorMedicalRequestList = () => {
                 </TableHead>
                 <TableBody>
                     {medicalRequests.map((request, index) => (
-                        <TableRow key={index}>
+                        <TableRow 
+                                key={index}
+                                hover
+                                onClick={() => handleRowClick(request)}
+                                style={{ cursor: 'pointer' }}
+                            >
                             <TableCell>{request.medicalrequest.requestName}</TableCell>
-                            <TableCell>{request.medicalrequest.requestDescription}</TableCell>
+                            <TableCell>
+                                <Typography noWrap sx={{ maxWidth: '200px', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                                    {request.medicalrequest.requestDescription}
+                                </Typography>
+                            </TableCell>
                             <TableCell>{request.medicalrequest.creationTime}</TableCell>
                             <TableCell>{request.medicalrequest.status}</TableCell>
                             <TableCell>
                                 <Tooltip title={`Chat with patient`}>
-                                    <Button onClick={() => handleChatClick()}><ChatIcon /></Button>
+                                    <Button onClick={(e) => handleChatClick(e)}><ChatIcon /></Button>
                                 </Tooltip>
                             </TableCell>
                             <TableCell>
                                 <Tooltip title="Delete Request">
-                                    <Button onClick={() => handleDeleteClick()}><DeleteIcon /></Button>
+                                    <Button onClick={(e) => handleDeleteClick(e, request.medicalrequest._id)}><DeleteIcon /></Button>
                                 </Tooltip>
                             </TableCell>
                         </TableRow>
@@ -78,6 +94,71 @@ const DoctorMedicalRequestList = () => {
                 </TableBody>
             </Table>
         </TableContainer>
+        <Modal
+                open={openModal}
+                onClose={handleCloseModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+                >
+                <Box sx={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        width: 600,
+                        bgcolor: 'background.paper',
+                        border: '2px solid #000',
+                        boxShadow: 24,
+                        p: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                    }}>
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            {selectedRequest? selectedRequest.medicalrequest.requestName: Request} Details
+                        </Typography>
+                        <IconButton aria-label="close" onClick={handleCloseModal}>
+                            <CloseIcon />
+                        </IconButton>
+                    </Box>
+                    {selectedRequest && (
+                        <>
+                        <Box sx={{ mt: 2 }}>
+                            <Typography variant="body1">{selectedRequest.medicalrequest.requestDescription}</Typography>
+                        </Box>
+                        <Box sx={{ mt: 2 }}>
+                            <Typography variant="body1">On {selectedRequest.medicalrequest.creationTime}</Typography>
+                        </Box>
+                        <Box sx={{ mt: 2 }}>
+                            <Typography variant="body1">Status: {selectedRequest.medicalrequest.status}</Typography>
+                        </Box>
+                        <Box sx={{ mt: 4 }}>
+                            <Input type="file" onChange={handleFileUpload} />
+                        </Box>
+                        <Box sx={{mt: 4}}>
+                            <Button
+                                variant="contained"
+                                startIcon={<ChatIcon />}
+                                onClick={(e) => handleChatClick(e)}
+                            >
+                                Chat with patient
+                            </Button>
+                        </Box>
+                        <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 2 }}>
+                            <Button
+                                variant="contained"
+                                color="error"
+                                startIcon={<DeleteIcon />}
+                                onClick={(e) => handleDeleteClick(e, selectedRequest.medicalrequest._id)}
+                            >
+                                Delete Request
+                            </Button>
+                        </Box>
+                    </>
+                    )}
+                </Box>
+            </Modal>
+        </>
     );
 };
 
--- app/src/components/lab/LabRequestList.jsx
+++ app/src/components/lab/LabRequestList.jsx
@@ -1,50 +1,57 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Tooltip, Button } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, 
+    TableRow, Paper, Tooltip, Button, Typography, Modal, Box, IconButton , Input} from '@mui/material';
 import ChatIcon from '@mui/icons-material/Chat';
-import GetAppIcon from '@mui/icons-material/GetApp';
 import DeleteIcon from '@mui/icons-material/Delete';
+import CloseIcon from '@mui/icons-material/Close';
 
 import labRequestService from "../../services/labrequestService";
-import labService from "../../services/labService";
 
 
-const LabRequestList = () => {
+const LabRequestList = ({fetchData, labRequests}) => {
 
-    const user = useSelector((state) => state.user);
-
-    const [labRequests, setLabRequests] = useState([]);
+    const [openModal, setOpenModal] = useState(false);
+    const [selectedRequest, setSelectedRequest] = useState(null);
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const labrequests = await labRequestService.getLabRequestsForLab(user.user.labId);
-                const labRequestsWithLabDetails = await Promise.all(
-                  labrequests.map(async labrequest => {
-                        const lab = await labService.getLab(labrequest.labId);
-                        return { labrequest, lab };
-                    })
-                );
-                console.log("labRequestsWithLabDetails : ", labRequestsWithLabDetails);
-                setLabRequests(labRequestsWithLabDetails);
-            } catch (error) {
-                console.error('Error fetching medical requests or doctors:', error);
-            }
-        };
-
-        fetchData();
+        fetchData("lab_lab_requests");
     }, []);
 
-    const handleChatClick = () => {
+    const handleChatClick = (e) => {
         console.log('Chat button clicked');
+        e.stopPropagation();
     };
 
-    const handleDeleteClick = () => {
+    const handleDeleteClick = async (e, labRequestId) => {
         console.log('Delete button clicked');
+        e.stopPropagation();
+        try {
+            const resp = await labRequestService.deleteLabRequest(labRequestId);
+            console.log("delete resp : ", resp);
+            fetchData("lab_lab_requests");
+        } catch (error) {
+            console.error('Error deleting lab request:', error);
+        }
+        setOpenModal(false);
+    };
+
+    const handleFileUpload = () => {
+        console.log('File Upload ');
+    };
+
+    const handleRowClick = (request) => {
+        setSelectedRequest(request);
+        setOpenModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+        setSelectedRequest(null);
     };
 
     return (
-        <TableContainer component={Paper} sx={{ marginTop: '30px' }}>
+        <>
+        <TableContainer component={Paper} sx={{ marginTop: '30px', maxHeight: '430px' }}>
             <Table aria-label="medical request table">
                 <TableHead>
                     <TableRow>
@@ -58,9 +65,18 @@ const LabRequestList = () => {
                 </TableHead>
                 <TableBody>
                     {labRequests.map((request, index) => (
-                        <TableRow key={index}>
+                        <TableRow 
+                            key={index}
+                            hover
+                            onClick={() => handleRowClick(request)}
+                            style={{ cursor: 'pointer' }}
+                            >
                             <TableCell>{request.labrequest.requestName}</TableCell>
-                            <TableCell>{request.labrequest.requestDescription}</TableCell>
+                            <TableCell>
+                                <Typography noWrap sx={{ maxWidth: '200px', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                                    {request.labrequest.requestDescription}
+                                </Typography>
+                            </TableCell>
                             <TableCell>{request.labrequest.createdDate}</TableCell>
                             <TableCell>{request.labrequest.status}</TableCell>
                             <TableCell>
@@ -70,7 +86,7 @@ const LabRequestList = () => {
                             </TableCell>
                             <TableCell>
                                 <Tooltip title="Delete Request">
-                                    <Button onClick={() => handleDeleteClick()}><DeleteIcon /></Button>
+                                    <Button onClick={() => handleDeleteClick(request.labrequest._id)}><DeleteIcon /></Button>
                                 </Tooltip>
                             </TableCell>
                         </TableRow>
@@ -78,6 +94,75 @@ const LabRequestList = () => {
                 </TableBody>
             </Table>
         </TableContainer>
+        <Modal
+                open={openModal}
+                onClose={handleCloseModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+                >
+                <Box sx={{
+                        position: 'absolute',
+                        top: '50%',
+                        left: '50%',
+                        transform: 'translate(-50%, -50%)',
+                        width: 600,
+                        bgcolor: 'background.paper',
+                        border: '2px solid #000',
+                        boxShadow: 24,
+                        p: 4,
+                        display: 'flex',
+                        flexDirection: 'column',
+                    }}>
+                    <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                        <Typography id="modal-modal-title" variant="h6" component="h2">
+                            {selectedRequest? selectedRequest.labrequest.requestName: Request} Details
+                        </Typography>
+                        <IconButton aria-label="close" onClick={handleCloseModal}>
+                            <CloseIcon />
+                        </IconButton>
+                    </Box>
+                    {selectedRequest && (
+                        <>
+                        {console.log("selectedRequest:",selectedRequest)}
+                        <Box sx={{mt: 2}}>
+                            <Typography variant="body1">Assigned to physician Dr. {selectedRequest.lab.name}</Typography>
+                        </Box>
+                        <Box sx={{ mt: 2 }}>
+                            <Typography variant="body1">{selectedRequest.labrequest.requestDescription}</Typography>
+                        </Box>
+                        <Box sx={{ mt: 2 }}>
+                            <Typography variant="body1">On {selectedRequest.labrequest.creationTime}</Typography>
+                        </Box>
+                        <Box sx={{ mt: 2 }}>
+                            <Typography variant="body1">Status: {selectedRequest.labrequest.status}</Typography>
+                        </Box>
+                        <Box sx={{ mt: 4 }}>
+                            <Input type="file" onChange={handleFileUpload} />
+                        </Box>
+                        <Box sx={{mt: 4}}>
+                            <Button
+                                variant="contained"
+                                startIcon={<ChatIcon />}
+                                onClick={(e) => handleChatClick(e)}
+                            >
+                                Chat with patient
+                            </Button>
+                        </Box>
+                        <Box sx={{ display: 'flex', justifyContent: 'flex-end', mt: 2 }}>
+                            <Button
+                                variant="contained"
+                                color="error"
+                                startIcon={<DeleteIcon />}
+                                onClick={(e) => handleDeleteClick(e, selectedRequest.labrequest._id)}
+                            >
+                                Delete Request
+                            </Button>
+                        </Box>
+                    </>
+                    )}
+                </Box>
+            </Modal>
+        </>
     );
 };
 
